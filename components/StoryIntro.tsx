b0be5ba43ddb6a0cc36106de1d5882950630f2ee
Vinzenz Schächner <137438216+SchVinzenz@users.noreply.github.com>
2026-05-29 12:22:53 +0200

    texte überarbietet

--- components/StoryIntro.tsx
+++ components/StoryIntro.tsx
@@ -7,21 +7,18 @@ export default function StoryIntro() {
 
         <div className="comic-panel panel stagger">
           <p style={{ fontSize: '1.05rem', lineHeight: 2, color: '#333' }}>
-            Vor zwei Jahren haben wir zusammen den <strong>deutschen Multimediapreis</strong> mit
-            unserem Projekt <strong>&bdquo;Meum Diarium – Ein Feldherr als Influencer&rdquo;</strong> gewonnen.
+            Vor zwei Jahren haben wir - Vinzenz und Benedikt Schächner - mit
+            unserem Projekt <strong>&bdquo;Meum Diarium – Ein Feldherr als Influencer&rdquo;</strong> zusammen den <strong>deutschen Multimediapreis</strong>  gewonnen.
             Hierfür haben wir eine Website erstellt, in der
-            <strong> Gaius Julius Caesar</strong> aus seiner subjektiven Sicht ein Tagebuch über
-            seine Feldzüge und andere Ereignisse geschrieben hat. Mit viel Liebe zum Detail haben
+            <strong> Gaius Julius Caesar</strong> aus seiner subjektiven Sicht ein unterhaltsames Tagebuch über
+            seine Feldzüge und andere tatsächlich stattgefundenen historischen Ereignisse geschrieben hat. Mit viel Liebe zum Detail haben
             wir unter anderem eine <strong>KI implementiert</strong>, die so antwortet, wie
             es Caesar vermutlich gemacht hätte.
           </p>
 
           <p className="mt-16" style={{ fontSize: '1.05rem', lineHeight: 2, color: '#333' }}>
-            Und genau dafür interessieren wir uns am meisten: Mit
-            <strong> neuen digitalen Medien</strong>, technischen Fortschritten und mit KI,
-            speziell aber auch im Bereich <strong>Self-Hosting</strong>. Wir haben zuhause von
-            Grund auf unseren eigenen Server mit eigener Cloud-Instanz &mdash; genauso wie
-            Google Drive oder iCloud &mdash; und mehreren Webseiten mit viel Liebe aufgebaut.
+            Genau das ist immer noch eine große Leidenschaft von uns: Egal ob wir privat etwas mit
+            <strong> digitalen Medien</strong> machen, als Technik-Team an unserer Schule Verantsaltungen organisieren oder selbst auf der Bühne stehen: Wir lieben es, Projekte umzusetzten oder uns über kreative Ideen auszutauschen.
           </p>
 
           <div
@@ -35,7 +32,7 @@ export default function StoryIntro() {
           />
 
           <p style={{ fontSize: '1.1rem', lineHeight: 1.8, color: 'var(--red)', fontWeight: 700, fontStyle: 'italic' }}>
-            Genau das – und hoffentlich auch diese Seite – zeigt am besten, wie sehr wir uns für neue, digitale Medien und kreative multimediale Projekte interessieren.
+            Als ehemalige Preisträger würden wir - Benedikt und Vinzenz - uns sehr freuen, wenn wir uns als Jugendbeirat in den Wettbewerb einbringen und mithelfen können. 
           </p>
         </div>
       </div>
